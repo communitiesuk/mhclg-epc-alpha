1975ea139911a51592c179620cbd54967ffbb9e8
Onkar Chiroma <>
2019-01-16 16:04:22 +0000

    Update routes.js
    Add test mock service

--- prototype-01/app/routes.js
+++ prototype-01/app/routes.js
@@ -15,6 +15,25 @@ const moment = require('moment')
 //   });
 // });
 
+router.get('/mock-api/address-grade', function(req, res, next) {
+  var contentType='mock-rest-api'
+  var contentId='20e71420-5c9d-4a8e-b9d6-093a0d772dab'
+  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
+  method: "GET",
+  headers: {
+      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
+    }
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send({ content : JSON.parse(body) });
+        // res.render('service-start', { content : JSON.parse(body) });
+        process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
+      } else {
+        res.redirect('/error');
+      }
+  });
+});
+
 router.get('/service-start-example', function(req, res, next) {
   var contentType='service-start'
   var contentId='d7fcda1d-d6d4-43d3-8cf4-b2af1ddce89f'
