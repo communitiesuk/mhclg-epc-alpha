e1da807791c09bf3d3758ca1486df9abf58a839b
Onkar Chiroma <>
2019-01-17 10:51:58 +0000

    Update config.js
    Update service name in line with initial wireframes

--- prototype-01/app/config.js
+++ prototype-01/app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find an Energy Performance Certificate',
+  serviceName: 'Energy performance of buildings register',
 
   // Default port that prototype runs on
   port: '3000',
