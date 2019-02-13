2498cde9216e0776a3ce0a9c5898727a49a26300
mcoulthurst <mc@lightness.co.uk>
2019-02-13 10:42:16 +0000

    Add basic CMS start page
    update routes to include error and service start pages

--- prototype-02/app/routes.js
+++ prototype-02/app/routes.js
@@ -1,6 +1,71 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
+const moment = require('moment')
 
 // Add your routes here - above the module.exports line
+
+
+// call start page from contomic CMS
+router.get('/', function(req, res, next) {
+  var contentType='article'
+  var contentId='ba19e506-b5d2-41e7-ab28-17eae0d1d79c'
+  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
+  method: "GET",
+  headers: {
+      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
+    }
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.render('article', { content : JSON.parse(body) });
+      } else {
+        res.redirect('/error');
+      }
+  });
+});
+
+
+router.get('/error', function(req, res, next) {
+  var today = moment(Date.now()).format('YYYY-MM-DD')
+  var tokenCreatedDate = moment(process.env.CONTOMIC_ACCESS_TOKEN_DATE, 'DD/MM/YYYY') 
+  var tokenExpiryDate = moment(tokenCreatedDate).add(30, 'days').format('YYYY-MM-DD')
+
+  if (moment(today).isAfter(tokenExpiryDate)){
+    res.render('error', { content : {error: {message: "Contomic trial expired"}}});
+  } else if (!process.env.CONTOMIC_ACCESS_TOKEN_DATE){
+  	res.render('error', { content : {error: {message: "CONTOMIC_ACCESS_TOKEN_DATE missing"}}});
+  } else {
+	res.render('error', { content : {error: {message: "Internal server error"}}});
+  }
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  SERVICE START
+//
+////////////////////////////////////////////////////////////////////////////
+
+router.get('/find-a-report', function(req, res, next) {
+  var contentType='service-start'
+  var contentId='434d4cc5-41fe-4b5c-b059-41c350f99d21'
+  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
+  method: "GET",
+  headers: {
+      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
+    }
+  }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.render('service-start', { content : JSON.parse(body) });
+        process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
+      } else {
+        res.redirect('/error');
+      }
+  });
+});
+
+
+
 
 module.exports = router
