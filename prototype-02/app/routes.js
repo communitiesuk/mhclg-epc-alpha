9d71152666917e4ac50ffe2a6c6f42fa1a4d3f46
mcoulthurst <mc@lightness.co.uk>
2019-02-28 11:14:35 +0000

    Add filter options
    add filter-date page with check boxes

--- prototype-02/app/routes.js
+++ prototype-02/app/routes.js
@@ -508,6 +508,63 @@ router.get('/filter', function(req, res, next) {
   });
 });
 
+router.get('/filter-date', function(req, res, next) {
+ var itemList = [
+    {
+      value: "2018",
+      text: "2018"
+    },
+    {
+      value: "2017",
+      text: "2017"
+    },
+    {
+      value: "2016",
+      text: "2016"
+    },
+    {
+      value: "2015",
+      text: "2015"
+    },
+    {
+      value: "2014",
+      text: "2014"
+    },
+    {
+      value: "2013",
+      text: "2013"
+    },
+    {
+      value: "2013",
+      text: "2013"
+    },
+    {
+      value: "2012",
+      text: "2012"
+    },
+    {
+      value: "2011",
+      text: "2011"
+    },
+    {
+      value: "2010",
+      text: "2010"
+    },
+    {
+      value: "2009",
+      text: "2009"
+    }
+
+  ]
+
+  res.render('auth/filter-date', {
+    title:"Date",
+    itemList:itemList,
+    filterList:filters,
+    links: availableOptions
+  });
+});
+
 
 router.get('/manage-accounts', function(req, res, next) {
   res.render('auth/manage-accounts', {
