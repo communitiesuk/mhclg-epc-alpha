0c0620d38536f536400a7ec37149eb57889e4ed0
mcoulthurst <mc@lightness.co.uk>
2019-03-18 15:47:55 +0000

    Clean up comments
    remove commented out code

--- prototype-01/app/routes.js
+++ prototype-01/app/routes.js
@@ -6,102 +6,11 @@ const _ = require('underscore');
 
 var dataset;
 
-
 router.get('/', function(req, res, next) {
   res.render('index', {  });
 });
 
-/*
-router.get('/epc-api-proxy/domestic/postcode/:postcode', function(req, res, next) {
-  request(process.env.EPC_API_URI+'?postcode='+req.params.postcode+'&size=150', {
-  method: "GET",
-  headers: {
-      'Authorization': process.env.EPC_API_KEY,
-      'Accept': 'application/json'
-    }
-  }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        if(body) {
-          // console.log(JSON.parse(body));
-          res.send({ content : JSON.parse(body) });
-          // res.render('service-start', { content : JSON.parse(body) });
-          // process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-        } else {
-          res.send('no data');
-        }
-      } else {
-        // res.redirect('/error');
-      }
-  });
-});
-*/
 
-
-/*
-router.get('/mock-api/address-grade', function(req, res, next) {
-  var contentType='mock-rest-api'
-  var contentId='20e71420-5c9d-4a8e-b9d6-093a0d772dab'
-  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
-  method: "GET",
-  headers: {
-      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-    }
-  }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send({ content : JSON.parse(body) });
-        // res.render('service-start', { content : JSON.parse(body) });
-        process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-      } else {
-        res.redirect('/error');
-      }
-  });
-});
-*/
-
-/*
-router.get('/service-start-example', function(req, res, next) {
-  var contentType='service-start'
-  var contentId='d7fcda1d-d6d4-43d3-8cf4-b2af1ddce89f'
-  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
-  method: "GET",
-  headers: {
-      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-    }
-  }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // res.send({ content : JSON.parse(body) });
-        res.render('service-start', { content : JSON.parse(body) });
-        process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-      } else {
-        res.redirect('/error');
-      }
-  });
-});
-*/
-
-/*
-router.get('/article-example', function(req, res, next) {
-  var contentType='article'
-  var contentId='952e678f-3c17-4b13-a16a-ddf2f21267bb'
-  request(process.env.CONTOMIC_CONTENT_API_URI+contentType+'/'+contentId, {
-  method: "GET",
-  headers: {
-      'Authorization': process.env.CONTOMIC_30_DAY_ACCESS_TOKEN
-    }
-  }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // console.log('body:', body);
-        // res.send({ content : JSON.parse(body) });
-        res.render('article', { content : JSON.parse(body) });
-      } else {
-        // console.log('error', error, response && response.statusCode);
-        // res.send('error', error, response && response.statusCode);
-        // return res.sendStatus(500);
-        res.redirect('/error');
-      }
-  });
-});
-*/
 router.get('/error', function(req, res, next) {
   res.render('error', { content : {error: {message: "Internal server error"}}});
 });
