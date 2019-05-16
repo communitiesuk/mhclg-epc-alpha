db27a9ce313362ad6dccb76d6f57984715d51223
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2019-05-16 15:01:13 +0100

    Add postcode lookup to find a certificate form

--- prototype-03/app/routes.js
+++ prototype-03/app/routes.js
@@ -13,4 +13,15 @@ router.get('/assessor/:id', (req, res) => {
   res.render('assessor/view', { assessor: req.session.data['assessors'][req.params.id] })
 })
 
+router.get('/cert-find-a-certificate', function(req, res, next) {
+  res.render('certificate/postcode-form');
+});
+
+router.post('/cert-postcode-results', function(req, res) {
+    res.redirect('certificate/postcode-results')
+});
+
+
+// module.exports should be the final line in the file
+
 module.exports = router
