e04b45e155e9b0024d1b6b6f3662c380db506b63
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2019-05-20 10:36:32 +0100

    Use fixed postcode for results; don't attempt API call

--- prototype-03/app/routes.js
+++ prototype-03/app/routes.js
@@ -23,90 +23,14 @@ var sortedArray = [];
 var lastCheckedPostcode = '';
 
 router.post('/cert-postcode-results', function(req, res) {
-  console.log(req.session.data);
   if(req.session.data['postcode-or-reference']){
-    var str = req.session.data['postcode-or-reference'];
-    var cleaned = str.split(' ').join('');
-    // flats/houses, Small Commercial Buildings, Large Commercial Buildings, Public buildings
-    if(cleaned!==lastCheckedPostcode){
-      // get data for address and modify to add dummy 'types'
-      request(process.env.EPC_API_URI+'?postcode='+cleaned+'&size=150', {
-        method: "GET",
-        headers: {
-            'Authorization': process.env.EPC_API_KEY,
-            'Accept': 'application/json'
-          }
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              if(body) {
-                dataset = JSON.parse(body);
-
-                lastCheckedPostcode = cleaned;
-
-                // sort by second property then first property
-                // sort by house number as main order then flat number
-                sortedArray = _.chain(dataset.rows)
-                  .sortBy('address1')
-                  .sortBy('address2')
-                  .value();
-
-                // loop through results and build a simple array
-                var arr = [];
-
-                for (var i=0;i<sortedArray.length;i++){
-                  //add in random certificate type
-                  var ran = Math.random()*10;
-                  var tempCertIdx = 1; //default is 1. 0 is for all certificates
-                  if(ran>9){
-                    tempCertIdx = 4;
-                  }
-                  else if(ran>8){
-                    tempCertIdx =3;
-                  }
-                  else  if(ran>6){
-                    tempCertIdx =2;
-                  }
-                  //update the sortedArray with the index for later sorting...
-                  sortedArray[i].certIndex = tempCertIdx;
-                  sortedArray[i].reference = sortedArray[i]['certificate-hash'];
-                  sortedArray[i].type = certTitles[tempCertIdx];
-                  sortedArray[i].initials = certInitials[tempCertIdx];
-                  sortedArray[i].address = sortedArray[i].address +', '+ dataset.rows[i].postcode;
-                  sortedArray[i].category = sortedArray[i]['current-energy-rating'];
-
-                }
-
-
-                filterCertTypes(req, res);
-
-
-              } else {
-                res.render('certificate/results', {
-                  addresses: []
-                });
-              }
-            } else {
-            // Error fetching results from API (e.g. because of Z-Scaler) but we can use canned data instead
-            console.log("FIRST: " + req.session.data['postcode-or-reference'])
-            req.session.data['postcode-or-reference'] = 'SW1P 3BU';
-            console.log("SECOND: " + req.session.data['postcode-or-reference'])
-            res.render('certificate/results', {
-                addresses: req.app.locals.data //static dummy data
-            });
-            // OR we could just... res.redirect('/error');
-            }
-        });
-
-    }else{
-      filterCertTypes(req, res);
-    }
-
+      res.render('certificate/results', {
+        addresses: req.app.locals.data //static dummy data
+      });
 
   }else{
-    // error reading data
-    res.send('no data');
+    filterCertTypes(req, res);
   }
-
 });
 
 
